ecb90ed90276341d1cd6fde6d8dd1046b7f91bd5
Artem-229 <artem321segun@gmail.com>
2026-03-12 18:54:48 +0300

    Update 000001_create_users.up.sql

--- backend/migrations/000001_create_users.up.sql
+++ backend/migrations/000001_create_users.up.sql
@@ -2,6 +2,5 @@ CREATE TABLE phones (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     fullname VARCHAR(255) NOT NULL,
     phone_number VARCHAR(10) CHECK (LENGTH(phone_number) = 10 AND phone_number ~ '^[0-9]+$') UNIQUE NOT NULL,
-    note VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    note VARCHAR(255) NOT NULL
 );
